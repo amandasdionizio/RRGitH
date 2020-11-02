0f7ea2ca873fb01d33ea697e16d7dfdad300de51
Amanda Salgado Dionizio <amanda.salgadodionizio@gmail.com>
2020-11-02 16:07:55 -0300

    Add functionality to generate csv and chose languages.

--- generator.py
+++ generator.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,32 +6,55 @@ import numpy as np
 from pathlib import Path
 from requests.exceptions import ConnectionError
 
+
 class Generator:
 
-    def __init__(self):
-        url = "https://api.github.com/search/repositories?q=language:python&sort=starts"
+    def __init__(self, languages):
+
+        self._languages = languages
+        print('Requesting data from repositories...')
+
         try:
-            self._requests = requests.get(url)
+            self._content = self._get_content()
         except ConnectionError:
-            self._requests = None
+            self._content = {}
             print(
-                "It was not possible to load data from the repositories on GitHub.\n"+
+                "It was not possible to load data from the repositories on GitHub.\n" +
                 "Please check your internet connection and try again."
             )
+        else:
+            print('Request made successfully.')
+
+    def _get_content(self):
+        url = "https://api.github.com/search/repositories?q=language:python&sort=starts"
+        content = requests.get(url).json()
+
+        items = content["items"]
+        for item in items:
+            languages_url = item["languages_url"]
+            languages = [l.lower() for l in requests.get(languages_url).json()]
+            if not all(language.lower() in languages for language in self._languages):
+                items.remove(item)
+
+        return content
 
     def _generate_image(self, file_name, x_values, y_values, bar_width, title):
         plt.rcParams['figure.figsize'] = (30, 30)
         plt.xticks(rotation=90)
         plt.yticks(np.arange(y_values[-1], y_values[0], 5000))
-        plt.bar(x_values, y_values, color = "green", width = bar_width)
+        plt.bar(x_values, y_values, color="green", width=bar_width)
         plt.suptitle(title)
-        plt.savefig(file_name.with_suffix('.png'), format ='png', dpi = 300)
-        plt.savefig(file_name.with_suffix('.svg'), format ='svg',dpi = 300)
+        plt.savefig(file_name.with_suffix('.png'), format='png', dpi=300)
+        plt.savefig(file_name.with_suffix('.svg'), format='svg', dpi=300)
         plt.close()
 
     def generate_chart(self):
-        content = self._requests.json()
-        items = content["items"]
+        if not self._content:
+            return
+
+        print('Generating charts...')
+
+        items = self._content["items"]
         names_values = []
         names = []
         values = []
@@ -50,4 +74,33 @@ class Generator:
         directory.mkdir(exist_ok=True)
 
         self._generate_image(directory / 'Popularity Repositories', names, values, 0.1, 'Popularity Repositories')
-        self._generate_image(directory / 'Six Most Popular Repositories', names[:6], values[:6], 0.6, 'Six Most Popular Repositories')
+        self._generate_image(directory / 'Six Most Popular Repositories', names[:6], values[:6], 0.6,
+                             'Six Most Popular Repositories')
+
+        print(f'Charts successfully generated in {str(directory)}')
+
+    def generate_csv(self):
+
+        if not self._content:
+            return
+
+        print('Generating CSV...')
+
+        items = self._content["items"]
+        columns = []
+
+        for item_column in items[0]:
+            columns.append(item_column)
+
+        home = Path.home()
+        directory = home / 'data'
+        directory.mkdir(exist_ok=True)
+
+        with open(directory / 'data.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
+            writer.writerow(columns)
+
+            for item in items:
+                writer.writerow(item.values())
+
+        print(f'CSV successfully generated in {str(directory)}')
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import argparse
 
 from generator import Generator
 
 if __name__ == '__main__':
-    generator = Generator()
+
+    parser = argparse.ArgumentParser(description='Example')
+
+    help_text = "Define the languages ​​that the projects should use, separated by a comma." + \
+    "E.g.: Python,HTML"
+
+    parser.add_argument(
+        "-l",
+        "--languages",
+        dest="languages",
+        type=str,
+        help=help_text,
+        default="Python",
+    )
+    parameters = parser.parse_args()
+    languages = parameters.languages.split(",")
+
+    generator = Generator(languages)
     generator.generate_chart()
+    generator.generate_csv()
